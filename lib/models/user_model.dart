b742b1747bb87dbc4e0918b57358fd5e756b4cbe
gustyxpower <gustiadityamuzaky08@gmail.com>
2025-07-09 02:10:55 +0700

    #33 Fix: Change Hive typeId for UserModel to 0
    The `typeId` for the `UserModel` Hive adapter and class definition has been changed from `1` to `0`. This ensures correct type registration with Hive.

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -2,7 +2,7 @@ import 'package:hive/hive.dart';
 
 part 'user_model.g.dart'; // untuk generate adapter Hive
 
-@HiveType(typeId: 1)
+@HiveType(typeId: 0)
 class UserModel extends HiveObject {
   @HiveField(0)
   final String email;
