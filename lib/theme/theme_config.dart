81293bc0e1dffbbf2e15c99fb0a44780374e3601
gustyxpower <gustiadityamuzaky08@gmail.com>
2025-07-09 20:13:27 +0700

    #46 Refactor: Improve theme configuration
    This commit refactors the theme configuration in `theme_config.dart`.
    
    - The `lightTheme` is now generated by a private helper function `_buildTheme`.
    - The `seedColor` for `ColorScheme.fromSeed` has been changed from `Colors.deepPurple` to `Colors.blue`.
    - The `_buildTheme` function takes a `Brightness` argument to potentially support dark themes in the future.
    
    Signed-off-by: gustyxpower <gustiadityamuzaky08@gmail.com>

--- lib/theme/theme_config.dart
+++ lib/theme/theme_config.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-final ThemeData lightTheme = ThemeData(
-  useMaterial3: true,
-  brightness: Brightness.light,
-  colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-  textTheme: GoogleFonts.poppinsTextTheme(),
-);
+final ThemeData lightTheme = _buildTheme(Brightness.light);
+
+ThemeData _buildTheme(Brightness brightness) {
+  final colorScheme = ColorScheme.fromSeed(
+    seedColor: Colors.blue,
+    brightness: brightness,
+  );
+
+  return ThemeData(
+    useMaterial3: true,
+    brightness: brightness,
+    colorScheme: colorScheme,
+    textTheme: GoogleFonts.poppinsTextTheme(),
+  );
+}
